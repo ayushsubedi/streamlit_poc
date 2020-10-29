0b81e1b4abebc2f20bb7d20a62d21acf697f15b1
Ayush Subedi <ayush.subedi@gmail.com>
2020-10-29 12:01:06 +0545

    resolve 1, resolve 2

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
+"""Main module for the streamlit app"""
 import streamlit as st
-import pandas as pd
-import numpy as np
+import pages.first, pages.second
 
-st.title('Uber pickups in NYC')
+PAGES = {
+    "Home": pages.first,
+    "Analysis1": pages.second,
+    # "Analysis2": pages.third,
+    # "Analysis3": pages.four,
+}
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+def main():
+    """Main function of the App"""
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+    page = PAGES[selection]
+    with st.spinner(f"Loading {selection} ..."):
+        page.write()
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+    st.sidebar.title("Sth else")
+    st.sidebar.info(
+        """
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        """
+    )
+    st.sidebar.title("Sth else")
+    st.sidebar.info(
+        """
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+        """
+    )
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+if __name__ == "__main__":
+    main()
